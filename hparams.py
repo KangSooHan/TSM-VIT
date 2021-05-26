74303b6e72c032bce6fdad9a577324edd66b7356
KangSooHan <tngkssapf@gmail.com>
2021-05-26 14:46:09 +0900

    modify code

--- hparams.py
+++ hparams.py
@@ -1,9 +1,14 @@
 import argparse
 
 class Hparams:
-    parser = argparser.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
     parser.add_argument('dataset', type=str)
-    parser.add_argument('modality', type=str, choice=['RGB', 'Flow'])
+    parser.add_argument('modality', type=str, choices=['RGB', 'Flow'])
+    parser.add_argument('--model_type', type=str, default='ViT-B_16', choices=['ViT-B_16', 'ViT-B_32', 'ViT-L_16',
+                                                                                'ViT-L_32', 'ViT-H_14'])
+    parser.add_argument('--local_rank', type=int, default=-1, help="local_rank for distributed training on gpus")
+    parser.add_argument('--img_size', type=int, default=224, help="Image Size")
 
     parser.add_argument('--save_dir', default="test", help="save directory")
+    parser.add_argument('--pretrained_dir', default="test", help="save directory")
--- train.py
+++ train.py
@@ -4,17 +4,20 @@ import time
 import argparse
 import logging
 import shutil
+import numpy as np
 from tqdm import tqdm
 
 import torch
 
-#from hparams import Hparams
+from hparams import Hparams
 #from utils import *
 from data_load import return_dataset
 #from model import *
 
+from models.model import VisionTransformer, CONFIGS
 
-def main(){
+
+def main():
     global hp
     logging.basicConfig(level = logging.INFO)
 
@@ -23,12 +26,33 @@ def main(){
     parser = hparams.parser
     hp = parser.parse_args()
 
+    # Setup CUDA, GPU & distributed training
+    if hp.local_rank == -1:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        hp.n_gpu = torch.cuda.device_count()
+    else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
+        torch.cuda.set_device(hp.local_rank)
+        device = torch.device("cuda", hp.local_rank)
+        torch.distributed.init_process_group(backend='nccl',
+                                             timeout=timedelta(minutes=60))
+        hp.n_gpu = 1
+    hp.device = device
+
+
+
+
     logging.info("# Prepare Dataset")
-    num_class, hp.train_list, hp.val_list, hp.root_path, prefix = dataset_config.return_dataset(hp.dataset, hp.modality)
+    num_class, hp.train_list, hp.val_list, hp.root_path, prefix = return_dataset(hp.dataset, hp.modality)
 
     save_dir = hp.save_dir
 
-    model = VisionTransformer(hp)
+    config = CONFIGS[hp.model_type]
+
+    model = VisionTransformer(config, hp.img_size, zero_head=True, num_classes=101)
+    model.load_from(np.load(hp.pretrained_dir))
+    model.to(hp.device)
+    num_params = count_parameters(model)
+
 
 if __name__ == '__main__':
     main()
